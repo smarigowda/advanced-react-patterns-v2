04c44932a1183cfb8e6b744426a40b116f2aa03f
Santosh Marigowda <santosharakere@gmail.com>
2018-06-18 22:10:03 +0100

    added wallaby js config

--- wallaby.js
+++ wallaby.js
@@ -2,7 +2,9 @@ module.exports = wallaby => {
   process.env.NODE_ENV = 'test';
   return {
     files: [
-      'src/exercises/**/*.+(js|jsx|json|snap|css|less|sass|scss|jpg|jpeg|gif|png|svg)',
+      'src/exercises/*.+(js|jsx|json|snap|css|less|sass|scss|jpg|jpeg|gif|png|svg)',
+      'src/switch.js',
+      'src/switch.styles.css',
       'test/*.js',
       '!src/__tests__/*.js'
     ],
