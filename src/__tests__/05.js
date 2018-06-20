8a2d47beca935743d47a9019a92760287e63c6e4
Santosh Marigowda <santosharakere@gmail.com>
2018-06-20 05:48:10 +0100

    feedback submitted

--- src/__tests__/05.js
+++ src/__tests__/05.js
@@ -27,6 +27,7 @@ test('can also toggle with the custom button', () => {
   expect(handleToggle).toHaveBeenCalledWith(true)
 })
 
+
 //////// Elaboration & Feedback /////////
 // When you've finished with the exercises:
 // 1. Copy the URL below into your browser and fill out the form
@@ -36,8 +37,8 @@ test('can also toggle with the custom button', () => {
 /*
 http://ws.kcd.im/?ws=react%20patterns&e=05&em=santosharakere@gmail.com
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
